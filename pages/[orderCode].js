85cb4ee071c52de13e7f039581dc6316e1658100
Dao Pham <daopm.dean@gmail.com>
2022-12-20 13:42:54 +0700

    order detail page

---
+++ pages/[orderCode].js
@@ -0,0 +1,26 @@
+export async function getStaticProps() {
+  const order = [];
+
+  return {
+    props: {
+      postData: order,
+    },
+  };
+}
+
+export default function OrderPage({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
