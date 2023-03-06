c61b974c4a0b1219414640eb85dbe2356f7eb3ba
Dao Pham <daopm.dean@gmail.com>
2023-03-06 22:40:11 +0700

    update prd link

--- common/constant.js
+++ common/constant.js
@@ -1,2 +1,3 @@
 // export const API_URL = "https://linknuoc-go-xhj5h5a4za-as.a.run.app";
-export const API_URL = "http://localhost:1314";
+export const API_URL = "https://plankton-app-af8md.ondigitalocean.app";
+// export const API_URL = "http://localhost:1314";
