cb443ab025ef6389da66f85f6a0262b8fa2a3dcd
DaoPM Dean <daopm.dean@gmail.com>
2024-07-26 13:47:12 +0700

    update

--- common/constant.js
+++ common/constant.js
@@ -1,3 +1,4 @@
 // export const API_URL = "https://linknuoc-go-xhj5h5a4za-as.a.run.app";
-export const API_URL = "https://sea-turtle-app-rfqsw.ondigitalocean.app";
+// export const API_URL = "https://sea-turtle-app-rfqsw.ondigitalocean.app";
+export const API_URL = "http://139.162.15.235:1314";
 // export const API_URL = "http://localhost:1314";
