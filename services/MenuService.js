b31e1491cceedf2fdd710732f795dd45e44cb3bc
DaoPM Dean <daopm.dean@gmail.com>
2025-06-09 00:41:04 +0700

    mockup menus

--- services/MenuService.js
+++ services/MenuService.js
@@ -1,15 +1,39 @@
 import { API_URL } from "../common/constant";
 
 export async function getMenusRes() {
-  try {
-    const menuFetchRes = await fetch(`${API_URL}/menus`);
-    return await menuFetchRes.json();
-  } catch (err) {
-    return {
-      status: "ERROR",
-      message: err.message,
-    };
-  }
+  // try {
+  //   const menuFetchRes = await fetch(`${API_URL}/menus`);
+  //   return await menuFetchRes.json();
+  // } catch (err) {
+  //   return {
+  //     status: "ERROR",
+  //     message: err.message,
+  //   };
+  // }
+
+  return {
+    status: 'SUCCESS',
+    data: [
+      {
+        id: 1,
+        menuName: 'Trà Sữa House',
+        menuLink: 'https://trasuahaus.com/menu',
+        username: 'alice',
+      },
+      {
+        id: 2,
+        menuName: 'Coffee Time',
+        menuLink: 'https://coffeetime.com/menu',
+        username: 'bob',
+      },
+      {
+        id: 3,
+        menuName: 'Juice Bar',
+        menuLink: 'https://juicebar.com/menu',
+        username: 'carol',
+      },
+    ],
+  };
 }
 
 export async function getMenuRes(menuCode) {
