99d74ed28c18e66baa3536e7e75e81490c76da34
DaoPM Dean <daopm.dean@gmail.com>
2025-10-12 16:45:36 +0700

    optimize: Layout LogoLink as client side

---
+++ components/LogoLink.js
@@ -0,0 +1,30 @@
+"use client";
+
+import { Typography } from '@mui/material';
+import { useRouter } from 'next/navigation';
+
+export default function LogoLink() {
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.push('/');
+  };
+
+  return (
+    <Typography 
+      variant="h6" 
+      component="div" 
+      onClick={handleLogoClick}
+      sx={{ 
+        color: 'primary.main', 
+        fontWeight: 'bold',
+        cursor: 'pointer',
+        '&:hover': {
+          opacity: 0.8
+        }
+      }}
+    >
+      Linknuoc
+    </Typography>
+  );
+}
--- components/layout.js
+++ components/layout.js
@@ -1,39 +1,16 @@
-"use client";
-
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
 import LanguageSwitcher from '../src/components/LanguageSwitcher';
+import LogoLink from './LogoLink';
 import styles from "./layout.module.css";
 import SEO from './SEO';
-import { useRouter } from 'next/navigation';
 
 export default function Layout({ children, home, seo }) {
-
-  const router = useRouter();
-
-  const handleLogoClick = () => {
-    router.push('/');
-  };
-
   return (
     <div className={styles.layoutWrapper}>
       <SEO {...seo} />
       <AppBar position="static" elevation={1} sx={{ bgcolor: 'background.paper', color: 'text.primary' }}>
         <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Typography 
-            variant="h6" 
-            component="div" 
-            onClick={handleLogoClick}
-            sx={{ 
-              color: 'primary.main', 
-              fontWeight: 'bold',
-              cursor: 'pointer',
-              '&:hover': {
-                opacity: 0.8
-              }
-            }}
-          >
-            Linknuoc
-          </Typography>
+          <LogoLink />
           <LanguageSwitcher />
         </Toolbar>
       </AppBar>
